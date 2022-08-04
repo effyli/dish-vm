1c9647559cb073354a6d13c524c66d4f2a7b19dd
Effy <effylix@gmail.com>
2022-08-04 17:51:31 +0200

    Optimize training and evaluating

--- paired_performance_measure.py
+++ paired_performance_measure.py
@@ -20,9 +20,10 @@ def get_f1(result_str, category=None):
         if category not in result_str:
             return None
         else:
-            return float(result_str.split(category)[1].split('      ')[3])
+
+            return result_str.split(category)[1].split('      ')[3]
     else:
-        return float(result_str.split('weighted avg')[1].split('      ')[3])
+        return result_str.split('weighted avg')[1].split('      ')[3]
 
 
 def encode_tags(tags, encodings, tag2id):
@@ -64,7 +65,7 @@ def evaluate(val_dataset, device, model, id2tag, categories=None):
         attention_mask = batch['attention_mask'].to(device)
         labels = batch['labels'].to(device)
         all_labels.append(labels.squeeze(0).tolist())
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        outputs = model(input_ids, attention_mask=attention_mask)
         logits = outputs.logits
         pred = torch.argmax(logits, dim=-1)
         all_preds.append(pred.squeeze(0).tolist())
@@ -74,6 +75,10 @@ def evaluate(val_dataset, device, model, id2tag, categories=None):
     preds_truncated = []
     # dealing with the ignored preds
     for labels, preds in zip(all_labels, all_preds):
+        if len(labels) != len(preds):
+            print('bad example')
+            logging.info('bad example')
+            continue
         arr_labels = np.array(labels)
         preds_truncated.append(np.array(preds)[arr_labels != -100].tolist())
         labels_truncated.append(arr_labels[arr_labels != -100].tolist())
@@ -125,69 +130,79 @@ def get_tag2id(labels):
     return tag2id, id2tag
 
 
-def trainer(train_data, train_labels, test_data, test_labels, weighted_f1s_categories, tag_id_maps, num_epoch=10, batch_size=32, model_name="dslim/bert-base-NER", evaluate_only=False):
+def dataset_loader(data, tokenizer, labels, tag2id):
+    encodings = tokenizer(data, is_split_into_words=True, return_offsets_mapping=True, padding=True,
+                                truncation=True)
+    labels = encode_tags(labels, encodings, tag2id)
+    encodings.pop('offset_mapping')
+    dataset = CustomizedDataset(encodings, labels)
+    return dataset
+
+
+def train(train_dataset, batch_size, model, num_epoch, device):
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    model.train()
+    optim = AdamW(model.parameters(), lr=5e-5)
+    for epoch in range(num_epoch):
+        print('Epoch ', epoch)
+        epoch_loss = 0
+        for batch in train_loader:
+            optim.zero_grad()
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs[0]
+            # print('Loss: ', loss.item())
+            epoch_loss += loss
+            loss.backward()
+            optim.step()
+        print('Epoch loss: ', epoch_loss)
+    return model
+
+
+def trainer(tag_id_maps, train_data=None, train_labels=None, test_data=None, test_labels=None, num_epoch=10,
+            batch_size=32, weighted_f1s_categories=None, model_name="dslim/bert-base-NER", mode='train', model=None):
     batch_size = batch_size
     num_epoch = num_epoch
     tag2id, id2tag = tag_id_maps
     # we fine-tune the model on train data and evaluate on test set
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    train_encodings = tokenizer(train_data, is_split_into_words=True, return_offsets_mapping=True, padding=True,
-                                truncation=True)
-    test_encodings = tokenizer(test_data, is_split_into_words=True, return_offsets_mapping=True, padding=True,
-                              truncation=True)
-
-    train_labels = encode_tags(train_labels, train_encodings, tag2id)
-    test_labels = encode_tags(test_labels, test_encodings, tag2id)
-
-    train_encodings.pop("offset_mapping")  # we don't want to pass this to the model
-    test_encodings.pop("offset_mapping")
-    train_dataset = CustomizedDataset(train_encodings, train_labels)
-    test_dataset = CustomizedDataset(test_encodings, test_labels)
-    print("test size:", len(test_dataset))
-    logging.info('test size: ' + str(len(test_dataset)))
-
-    if evaluate_only:
-        model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=len(tag2id), ignore_mismatched_sizes=True)
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-        model.to(device)
-        model.eval()
-        weighted_f1, all_reports = evaluate(test_dataset, device, model, id2tag, weighted_f1s_categories)
-        return weighted_f1, all_reports
-
+    if mode == 'train':
+        model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=len(tag2id),
+                                                                ignore_mismatched_sizes=True)
     else:
+        model = model
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+    tag2id_model = model.config.label2id
+    for tag, id in tag2id.items():
+        if tag not in tag2id_model.keys():
+            tag2id_model[tag] = len(tag2id_model)
+    tag2id = tag2id_model
+    id2tag = {id: tag for tag, id in tag2id.items()}
 
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
-        # Load model
-        model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=len(tag2id), ignore_mismatched_sizes=True)
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-        model.to(device)
-        model.train()
-
-        optim = AdamW(model.parameters(), lr=5e-5)
-
-        for epoch in range(num_epoch):
-            print('Epoch ', epoch)
-            epoch_loss = 0
-            for batch in train_loader:
-                optim.zero_grad()
-                input_ids = batch['input_ids'].to(device)
-                attention_mask = batch['attention_mask'].to(device)
-                labels = batch['labels'].to(device)
-                outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-                loss = outputs[0]
-                # print('Loss: ', loss.item())
-                epoch_loss += loss
-                loss.backward()
-                optim.step()
-            print('Epoch loss: ', epoch_loss)
-
+    if mode == 'evaluate':
+        if test_data == None or test_labels == None:
+            print('Need to provide test data')
+            logging.info('Need to provide test data')
+            raise ValueError
+        test_dataset = dataset_loader(test_data, tokenizer, test_labels, tag2id)
+        print("test size:", len(test_dataset))
+        logging.info('test size: ' + str(len(test_dataset)))
+        model.eval()
         weighted_f1, result_str, weighted_f1s_categories = evaluate(test_dataset, device, model, id2tag, weighted_f1s_categories)
-        # TODO: dump all_reports to log file
         print(result_str)
+        logging.info(result_str)
         return weighted_f1, result_str, weighted_f1s_categories
+
+    elif mode == 'train':
+        model.train()
+        train_dataset = dataset_loader(train_data, tokenizer, train_labels, tag2id)
+        print("train size:", len(train_dataset))
+        logging.info('train size: ' + str(len(train_dataset)))
+        model = train(train_dataset, batch_size, model, num_epoch, device=device)
+        return model
 
 
 if __name__ == '__main__':
@@ -225,46 +240,37 @@ if __name__ == '__main__':
     print("Number of datasets: ", len(datasets))
 
     results = {}
+    epoch = 10
     # all_reports = []
-    # using Cartesian product to get all combinations with repetition
-    for p_names in itertools.product(names, repeat=2):
-        # getting the source data name and the target data name
-        # we assume the first one is the source domain and the latter is target domain
-        s_name, t_name = p_names[0], p_names[1]
-        print("Processing dataset {} and {}".format(s_name, t_name))
-        logging.info('Paired datasets names: {} and {}'.format(s_name, t_name))
-        print('loading data')
-        weighted_f1_categories = {'ORG': [], 'PER': [], 'DIG': [], 'LOC': []}
-        if s_name != t_name:
-            s_train_data, s_train_labels, s_test_data, s_test_labels = load_data(data_folder, s_name)
-            t_train_data, t_train_labels, t_test_data, t_test_labels = load_data(data_folder, t_name)
-
-            tag2id, id2tag = get_tag2id(s_train_labels + t_test_labels)
-            # test_tag2id, test_id2tag = get_tag2id(t_test_labels)
-
+    # we do a two-sided paring experiments, so for some distance, we have two data points for the performance difference
+    for s_name in names:
+        # we get the source dataset, and fine-tune on the source training set, we then evaluate on all the test sets
+        print('Processing source dataset {}'.format(s_name))
+        logging.info('Processing source dataset {}'.format(s_name))
+        logging.info('loading data')
+        s_train_data, s_train_labels, _, _ = load_data(data_folder, s_name)
+        tag2id, id2tag = get_tag2id(s_train_labels)
+        if DEBUG:
+            s_train_data, s_train_labels = s_train_data[10:20], s_train_labels[10:20]
+            epoch = 1
+        model = trainer((tag2id, id2tag), train_data=s_train_data, train_labels=s_train_labels, num_epoch=epoch, mode='train')
+        for t_name in names:
+            # getting the target dataset that we wants to evaluate on
+            # we keep the source dataset fixed this way, fine-tune on the source dataset and evaluate on the rest of the datasets
+            print("Processing dataset {} and {}".format(s_name, t_name))
+            logging.info('Paired datasets names: {} and {}'.format(s_name, t_name))
+            print('loading data')
+            weighted_f1_categories = {'ORG': [], 'PER': [], 'DIG': [], 'LOC': []}
+            # we then evaluate the fine-tuned model on the test set of the target dataset
+            _, _, t_test_data, t_test_labels = load_data(data_folder, t_name)
+            tag2id, id2tag = get_tag2id(t_test_labels)
             if DEBUG:
-                s_train_data, s_train_labels, s_test_data, s_test_labels = s_train_data[:10], s_train_labels[:10], s_test_data[:10], s_test_labels[:10]
-                t_train_data, t_train_labels, t_test_data, t_test_labels = t_train_data[:10], t_train_labels[:10], t_test_data[:10], t_test_labels [:10]
-            epoch = 1 if DEBUG else 10
+                t_test_data, t_test_labels = t_test_data[10:20], t_test_labels[10:20]
+            weighted_f1, result_str, weighted_f1s_categories = trainer((tag2id, id2tag), test_data=t_test_data,
+                                                                       test_labels=t_test_labels, weighted_f1s_categories=weighted_f1_categories, mode='evaluate', model=model)
 
-            weighted_f1, result_str, weighted_f1s_categories = trainer(s_train_data, s_train_labels, t_test_data,
-                                                                       t_test_labels, weighted_f1_categories,
-                                                                       (tag2id, id2tag),
-                                                                       num_epoch=epoch)
-            # all_reports.append((weighted_f1, result_str, weighted_f1s_categories))
-        else:
-            s_train_data, s_train_labels, t_test_data, t_test_labels = load_data(data_folder, s_name)
-            tag2id, id2tag = get_tag2id(s_train_labels + t_test_labels)
-            # test_tag2id, test_id2tag = get_tag2id(t_test_labels)
-            if DEBUG:
-                s_train_data, s_train_labels, t_test_data, t_test_labels = s_train_data[:10], s_train_labels[:10], t_test_data[:10], t_test_labels[:10]
-            epoch = 1 if DEBUG else 10
-            weighted_f1, result_str, weighted_f1s_categories = trainer(s_train_data, s_train_labels, t_test_data,
-                                                                       t_test_labels, weighted_f1_categories, (tag2id, id2tag),
-                                                                       num_epoch=epoch)
-            # all_reports.append((weighted_f1, result_str, weighted_f1s_categories))
-        name_key = s_name + ' ' + t_name
-        results[name_key] = (weighted_f1, result_str, weighted_f1s_categories)
+            name_key = s_name + ' ' + t_name
+            results[name_key] = (weighted_f1, result_str, weighted_f1s_categories)
 
     with open(output_folder + 'paird_perf_measure', 'a+') as f:
         json.dump(results, f)
