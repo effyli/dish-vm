18eb12ee520cfb03f2a7563e3482e3c7afe65d89
Effy <effylix@gmail.com>
2022-12-11 15:24:09 +0100

    adding label mapping

--- ner-ft-test.py
+++ ner-ft-test.py
@@ -20,8 +20,9 @@ def compute_metrics(p):
     predictions = np.argmax(predictions, axis=2)
 
     # Remove ignored index (special tokens)
+    # True prediction use label_list_model; true labels use label_list_data
     true_predictions = [
-        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        [label_list_model[p] for (p, l) in zip(prediction, label) if l != -100]
         for prediction, label in zip(predictions, labels)
     ]
     true_labels = [
@@ -38,6 +39,7 @@ def compute_metrics(p):
     }
 
 def tokenize_and_align_labels(examples):
+    # print("pre-processing")
     tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
     label_all_tokens = True
     labels = []
@@ -52,7 +54,7 @@ def tokenize_and_align_labels(examples):
                 label_ids.append(-100)
             # We set the label for the first token of each word.
             elif word_idx != previous_word_idx:
-                label_ids.append(label[word_idx])
+                label_ids.append(label_map[label[word_idx]])
             # For the other tokens in a word, we set the label to either the current label or -100, depending on
             # the label_all_tokens flag.
             else:
@@ -64,22 +66,44 @@ def tokenize_and_align_labels(examples):
     tokenized_inputs["labels"] = labels
     return tokenized_inputs
 
+# second processing when model labels are different from dataset labels
+def map_labels(examples):
+    tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+    labels = []
+    for i, label in enumerate(examples[f"{task}_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+
 if __name__ == '__main__':
-    datasets_train = load_dataset("conll2003", split="train")
-    datasets_val = load_dataset("conll2003", split="validation")
-    datasets = {"train": datasets_train, "val": datasets_val}
+    seed = 2022
+    transformers.set_seed(seed)
+
+    datasets = load_dataset("conll2003")
 
     # this label list is different from the actual label list that model was trained on
-    label_list_data = datasets_train.features[f"{task}_tags"].feature.names
-    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-    data_collator = DataCollatorForTokenClassification(tokenizer)
-    tokenized_datasets_train = datasets_train.map(tokenize_and_align_labels, batched=True)
-    tokenized_datasets_val = datasets_val.map(tokenize_and_align_labels, batched=True)
-
+    # we have to deal with the label mapping
+    label_list_data = datasets['train'].features[f"{task}_tags"].feature.names
+    label_dict_data = {k: v for k, v in enumerate(label_list_data)}
     model = AutoModelForTokenClassification.from_pretrained(model_checkpoint, num_labels=len(label_list_data))
-    label_dict = model.config.id2label
-    # sort dictionary by keys
-    label_list = [i[1] for i in sorted(label_dict.items(), key=lambda x:x[0])]
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, grouped_entities=True, ignore_subwords=True)
+
+    label_dict_model = model.config.id2label
+    label_dict_rev_model = model.config.label2id
+    label_list_model = [v for k, v in sorted(label_dict_model.items(), key=lambda k:k[0])]
+    # need matching
+    label_map = {}
+    if label_dict_data != label_dict_model:
+        # we map the dataset label to model label
+        for k, v in label_dict_data.items():
+            k_model = label_dict_rev_model[v]
+            label_map[k] = k_model
+    else:
+        for i in range(len(label_list_data)):
+            label_map[i] = i
+
+    tokenized_datasets = datasets.map(tokenize_and_align_labels, batched=True)
+
+    data_collator = DataCollatorForTokenClassification(tokenizer)
+    # when compute metrics, we use two label dict to convert label_ids
 
     model_name = model_checkpoint.split("/")[-1]
     args = TrainingArguments(
@@ -96,25 +120,34 @@ if __name__ == '__main__':
     data_collator = DataCollatorForTokenClassification(tokenizer)
     metric = load_metric("seqeval")
 
+
     trainer = Trainer(
         model,
         args,
-        train_dataset=datasets_train,
-        eval_dataset=datasets_val,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["validation"],
         data_collator=data_collator,
         tokenizer=tokenizer,
         compute_metrics=compute_metrics
     )
 
-    # trainer.train()
-    # trainer.evaluate()
+    trainer.train()
+    trainer.evaluate()
 
-    predictions, labels, _ = trainer.predict(tokenized_datasets_val)
-    predictions = np.argmax(predictions, axis=2)
+    testing_example = "My name is Effy, and I am from Jiaozuo, Henan."
+    print("after tokenization: ", tokenizer(testing_example))
+    print("coming back to words: ", tokenizer.convert_ids_to_tokens(tokenizer(testing_example)["input_ids"]))
+
+
+    # examples = load_dataset("conll2003", split="validation[0:16]")
+    #
+    # examples = examples.map(tokenize_and_align_labels, batched=True)
+    # predictions, labels, _ = trainer.predict(examples)
+    # predictions = np.argmax(predictions, axis=2)
 
     # Remove ignored index (special tokens)
     true_predictions = [
-        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        [label_list_model[p] for (p, l) in zip(prediction, label) if l != -100]
         for prediction, label in zip(predictions, labels)
     ]
     true_labels = [
@@ -124,6 +157,6 @@ if __name__ == '__main__':
 
     results = metric.compute(predictions=true_predictions, references=true_labels)
     print(results)
-    # trainer.push_to_hub()
+    trainer.push_to_hub()
 
     wandb.finish()
