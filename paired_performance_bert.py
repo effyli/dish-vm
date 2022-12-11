4152af4678358a4427e79cc50b6f26266f1e53ac
Effy <effylix@gmail.com>
2022-12-11 20:11:11 +0100

    fixing minor bugs

--- paired_performance_bert.py
+++ paired_performance_bert.py
@@ -40,33 +40,7 @@ def compute_metrics(p):
         "accuracy": results["overall_accuracy"],
     }
 
-def tokenize_and_align_labels(examples):
-    label_all_tokens = True
-    tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
 
-    labels = []
-    for i, label in enumerate(examples[f"{task}_tags"]):
-        word_ids = tokenized_inputs.word_ids(batch_index=i)
-        previous_word_idx = None
-        label_ids = []
-        for word_idx in word_ids:
-            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
-            # ignored in the loss function.
-            if word_idx is None:
-                label_ids.append(-100)
-            # We set the label for the first token of each word.
-            elif word_idx != previous_word_idx:
-                label_ids.append(label_map[label[word_idx]])
-            # For the other tokens in a word, we set the label to either the current label or -100, depending on
-            # the label_all_tokens flag.
-            else:
-                label_ids.append(label_map[label[word_idx]] if label_all_tokens else -100)
-            previous_word_idx = word_idx
-
-        labels.append(label_ids)
-
-    tokenized_inputs["labels"] = labels
-    return tokenized_inputs
 
 
 def load_data_from_json(data_folder, name, mode):
@@ -130,6 +104,36 @@ if __name__ == '__main__':
         print(label_list)
         tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
         assert isinstance(tokenizer, transformers.PreTrainedTokenizerFast)
+
+        def tokenize_and_align_labels(examples):
+            label_all_tokens = True
+            tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+
+            labels = []
+            for i, label in enumerate(examples[f"{task}_tags"]):
+                word_ids = tokenized_inputs.word_ids(batch_index=i)
+                previous_word_idx = None
+                label_ids = []
+                for word_idx in word_ids:
+                    # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+                    # ignored in the loss function.
+                    if word_idx is None:
+                        label_ids.append(-100)
+                    # We set the label for the first token of each word.
+                    elif word_idx != previous_word_idx:
+                        label_ids.append(label_map[label[word_idx]])
+                    # For the other tokens in a word, we set the label to either the current label or -100, depending on
+                    # the label_all_tokens flag.
+                    else:
+                        label_ids.append(label_map[label[word_idx]] if label_all_tokens else -100)
+                    previous_word_idx = word_idx
+
+                labels.append(label_ids)
+
+            tokenized_inputs["labels"] = labels
+            return tokenized_inputs
+
+
         tokenized_train_dataset = train_dataset.map(tokenize_and_align_labels, batched=True)
         tokenized_test_dataset = test_dataset.map(tokenize_and_align_labels, batched=True)
         model = AutoModelForTokenClassification.from_pretrained(model_checkpoint, num_labels=len(label_list))
@@ -164,7 +168,7 @@ if __name__ == '__main__':
 
         for t_name in names:
             print("Processing dataset {} and {}".format(s_name, t_name))
-            test_dataset = load_dataset(data_folder, t_name, 'test')
+            test_dataset = load_data_from_json(data_folder, t_name, 'test')
             tokenized_test_dataset = test_dataset.map(tokenize_and_align_labels, batched=True)
             trainer.evaluate(tokenized_test_dataset)
 
